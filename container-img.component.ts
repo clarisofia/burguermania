d80633d2a8425026a3ea10e54fabfc5cc269953a
clarisofia <clarisofiasz@gmail.com>
2024-11-21 00:07:05 -0300

    Add files via upload

---
+++ container-img.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-container-img',
+  imports: [],
+  templateUrl: './container-img.component.html',
+  styleUrl: './container-img.component.css'
+})
+export class ContainerImgComponent {
+
+}
